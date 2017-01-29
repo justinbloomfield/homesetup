4a950f686cd6c81c80e9370517847c161d79a7c9
Justin Bloomfield <jbloomfield@live.com>
2017-01-29 21:14:48 +1100

    guess it works

--- setup-home.sh
+++ setup-home.sh
@@ -1,9 +1,34 @@
 #!/bin/sh
 
 echo -n "Packager: "
-read PACKAGER
-echo $PACKAGER
+read PKGR
+echo $PKGR
 echo -n "Packager install command: "
-read PACKAGER_INSTALL
-echo $PACKAGER_INSTALL
- 
+read PKGR_INST
+echo $PKGR_INST
+
+PKGGET="sudo $PKGR $PKGR_INST"
+DFURL='https://github.com/justinbloomfield/dotfiles'
+
+getinitpkg () {
+    $PKGGET git stow 
+}
+
+createdirs () {
+    mkdir -p ~/etc && touch ~/etc/info
+    mkdir -p ~/src/{pkg,go,hsk,c}
+    mkdir -p ~/usr/{dl,img,vid,doc}
+    mkdir -p ~/var/{mail,fifo,logs,tox}
+}
+
+dotfiles () {
+    git clone $DFURL ~/.df
+    cd ~/.df
+    mv README.md ..
+    stow *
+    mv ../README.md .
+}
+
+getinitpkg
+createdirs
+dotfiles
